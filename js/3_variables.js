e46244e64e3081755f57eeda803f0de1a517605e
wesleymarchi <wesleymarchi.x3@gmail.com>
2023-01-03 14:14:31 -0300

    Definindo variáveis.

--- js/3_variables.js
+++ js/3_variables.js
@@ -1,20 +1,28 @@
 // Definindo uma variável sem nenhuma atribuição.
 var zero;
 
-// Definindo uma variável do tipo string
+// Definindo uma variável do tipo string.
 var message = "Hello world!";
 
-// Definindo uma variável do tipo integer
+// Definindo uma variável do tipo integer.
 var num = 10;
 
-// Definindo uma variável do tipo decimal
+// Definindo uma variável do tipo decimal.
 var pi = 3.14;
 
-// Definindo uma variável do tipo boolean
+// Definindo uma variável do tipo boolean.
 var flag = true; // true ou false
+
+// Definindo um array vazio.
+var books = [];
+
+// Definindo um array.
+var colors = ['red', 'green', 'blue'];
 
 print("Variável zero: ", zero);
 print("Variável message: ", message);
 print("Variável num: ", num);
 print("Variável pi: ", pi);
 print("Variável flag: ", flag);
+print("Variável books: ", books);
+print("Variável colors: ", colors);
