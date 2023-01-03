402fe66905af316e3075a1d1ebd38d5218099a95
wesleymarchi <wesleymarchi.x3@gmail.com>
2023-01-03 13:33:01 -0300

    Definindo variáveis.

--- js/3_variables.js
+++ js/3_variables.js
@@ -1,3 +1,6 @@
+// Definindo uma variável sem nenhuma atribuição.
+var zero;
+
 // Definindo uma variável do tipo string
 var message = "Hello world!";
 
@@ -9,3 +12,9 @@ var pi = 3.14;
 
 // Definindo uma variável do tipo boolean
 var flag = true; // true ou false
+
+print("Variável zero: ", zero);
+print("Variável message: ", message);
+print("Variável num: ", num);
+print("Variável pi: ", pi);
+print("Variável flag: ", flag);
