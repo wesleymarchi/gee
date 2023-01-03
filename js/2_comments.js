34784ac6a32dab33dd982d08723138b480c6abb9
wesleymarchi <wesleymarchi.x3@gmail.com>
2023-01-03 12:00:38 -0300

    Desenvolvidos os primeiros scripts.

---
+++ js/2_comments.js
@@ -0,0 +1,6 @@
+// Comentário com duas barras invertidas.
+
+/*
+Comentário com barra e asterisco.
+ */
+print('Hello world!');
---
+++ js/3_variables.js
@@ -0,0 +1,11 @@
+// Definindo uma variável do tipo string
+var message = "Hello world!";
+
+// Definindo uma variável do tipo integer
+var num = 10;
+
+// Definindo uma variável do tipo decimal
+var pi = 3.14;
+
+// Definindo uma variável do tipo boolean
+var flag = true; // true ou false
