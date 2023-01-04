22675cd7550feb9981b1711529b01fb40e48c83a
wesleymarchi <wesleymarchi.x3@gmail.com>
2023-01-04 10:17:50 -0300

    Expressões e Operadores.

--- js/5_expression_operator.js
+++ js/5_expression_operator.js
@@ -1,1 +1,29 @@
-// Expressões e operadores.
+/* Expressões e Operadores */
+
+var price = 10;
+var qty = 3;
+var total = price * qty;
+var num = 100;
+var street = 'JK';
+var add = street + num; // Agora num é uma string em add.
+
+print("Adição: ", 10 + 2);
+print("Subtração: ", 10 - 2);
+print("Divisão: ", 10 / 2);
+print("Multiplicação: ", 10 * 2);
+
+// Adiciona 1 do valor atual.
+print("Incremento: ", ++price);
+
+// Subtrai 1 do valor atual.
+print("Decremento: ", --price);
+
+// Operador de string.
+print("Uni as strings: ", add);
+print("Hello " + "world!");
+
+// Divide e retorna o resto da divisão.
+print("Módulo: ", 10 % 3);
+
+print("5 + 10 * 2 = ?", 5 + 10 * 2);
+print("Total de uma compra: ", total);
