1b2b656471b348736114affbb38d571dd415ac38
wesleymarchi <wesleymarchi.x3@gmail.com>
2023-01-05 13:39:03 -0300

    Objetos e métodos.

--- js/7_objects.js
+++ js/7_objects.js
@@ -9,7 +9,7 @@ var car = {
     tankSize: 14, // Liters
     gasStatus: 10, // Liters
     checkGas: function() {
-        return this.gasStatus;
+        return (this.gasStatus / this.tankSize) * 100; // Percentual de combustivel no tanque.
     }
 };
 
@@ -24,13 +24,13 @@ motorcycle['year'] = 2020;
 
 // LIMPANDO UMA PROPRIEDADE E DELETANDO UMA PROPRIEDADE
 motorcycle.brand = '';
-motorcycle.brand;
+delete motorcycle.brand;
 
 // ACESSANDO OBJETO COM NOTAÇÃO DE PONTO E COLCHETES
 var carModel = car.model; // Notação de ponto.
 var carGasStatus = car.checkGas();
 
 print("Modelo: ", carModel);
-print("Status de combustivel: ", carGasStatus);
+print("Status de combustivel %: ", carGasStatus);
 print("Modelo da moto: ", motorcycle['model']); // Colchetes.
-print("Ano da moto: ", motorcycle['year']);
+print("Ano da moto: ", motorcycle.year);
