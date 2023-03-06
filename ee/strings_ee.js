828c10c331d3b87970e99abf0de43cf0d4700ae5
wesleymarchi <wesleymarchi.x3@gmail.com>
2023-03-06 10:05:06 -0300

    Definindo uma string local e enviando para o Google processar.

---
+++ ee/strings_ee.js
@@ -0,0 +1,5 @@
+/* DEFINE UMA STRING E, EM SEGUIDA, INSERE EM UM CONTAINER PARA O GOOGLE PROCESSAR */
+var clientString = 'Google.';
+var serverString = ee.String(clientString);
+
+print('Estou no ' + serverString);
