5e1bf7a943e5555653cfcb4b7908fea9c381c012
wesleymarchi <wesleymarchi.x3@gmail.com>
2023-03-15 14:05:50 -0300

    Gerando uma lista a partir de um objeto EE.

---
+++ ee/3_lists_ee.js
@@ -0,0 +1,10 @@
+/* UTILIZANDO EE.LIST() para gerar um objeto lista do EE */
+
+// Duas formas de instanciar um objeto lista do EE
+var serverList = ee.List([1, 2, 3, 4, 5]);
+
+// Gerando uma sequência a partir do método sequence() do EE
+var sequence = ee.List.sequence(1, 5);
+
+print('List JS: ', serverList);
+print('Sequence: ', sequence);
