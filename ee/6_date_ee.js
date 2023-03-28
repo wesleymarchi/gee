059c7c078ca5aaa22cbfaa3ba5ee9a1ff5aef48e
wesleymarchi <wesleymarchi.x3@gmail.com>
2023-03-28 17:00:10 -0300

    Utilizando o objeto ee.Date().

--- ee/6_date_ee.js
+++ ee/6_date_ee.js
@@ -4,7 +4,7 @@ print('Server date: ', dateServer);
 
 // Obtém a data atual com o objeto JS
 var dateClient = Date.now();
-print('Milisegundos desde 1 de janeiro de 1970: ', dateClient);
+print('Milissegundos desde 1 de janeiro de 1970: ', dateClient);
 
 // Instanciando um objeto ee.Date
 var eeDate = ee.Date(dateClient);
